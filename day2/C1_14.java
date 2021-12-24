303cd2b4aedf2fe6f5f95b8cde13fc65592fab6b
hogehoge <susumitosusyo@hotmail.com>
2021-12-24 11:04:02 +0900

    C-14,code 1_14の作成 python でのキーボード入力

---
+++ day2/C1_14.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class C1_14{
+public static void main(String[]args)
+	Scanner sc =new Scanner(System.in);
+	Sytem.out.println("名前ください");
+	String name=sc.nextLine();
+	System.out.printf("おお%sがくるのを待っておったぞ。",name);
+}
+}

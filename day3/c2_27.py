c0c5bedca45572a1175d952c9ef92bd5e9654b60
hogehoge <susumitosusyo@hotmail.com>
2021-12-27 14:02:37 +0900

    c2_27セットの席集合

---
+++ day3/c2_27.py
@@ -0,0 +1,7 @@
+member_hobbies={
+                '松田':{'SNS','麻雀','自転車'},
+                '浅黄':{'食べ歩き','麻雀','自転車','自転車}
+                }
+    common_hobbies=member_hobbies['松田']&member_hobbies['浅黄']
+    print(common=hobbies)
+

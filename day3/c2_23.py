607ae8a8843ace9669ca4a9636c5b00ad1a59178
hogehoge <susumitosusyo@hotmail.com>
2021-12-27 13:00:47 +0900

    c2_23　追加 コレクションの相互変換

---
+++ day3/c2_23.py
@@ -0,0 +1,9 @@
+scores ={'network' :60,'database':80,'security':60}
+members=['Jonny','jim','Henry']
+print(tuple(members))
+print(list(scores))
+print(set(scores.values()))
+color_en=['red','green','blue']
+color_ja=['赤','緑','青']
+color=dict(zip(color_en,color_ja))
+print(color)

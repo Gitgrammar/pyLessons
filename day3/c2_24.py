9b1fe6ce1d8a01e24f8530bde31ccd22c8e04f8b
hogehoge <susumitosusyo@hotmail.com>
2021-12-27 13:35:48 +0900

    c2_24 ディクショナリのネスト

---
+++ day3/c2_24.py
@@ -0,0 +1,8 @@
+matsuda_scores={'network':60,'database':80,'security':50}
+asagi_scores={'network':56,'database':70,'security':58}
+member_scores={
+        '松田':matsuda_scores,
+        '浅木':asagi_scores,
+        
+              }
+print(member_scores)

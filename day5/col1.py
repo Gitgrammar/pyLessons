deaadc7476f14ce53475bc54c2652d5d282b99fc
hogehoge <susumitosusyo@hotmail.com>
2022-01-06 12:36:30 +0900

    c4_5追加 while文とリスト

--- day5/col1.py
+++ day5/col1.py
@@ -4,5 +4,5 @@ n =random.randint(1,10)
 div ='偶数' if n % 2==0 else'奇数'
 print(f'{n}は{div}です。')
 
-result='優' if n>=9 else '良' n>=6 if n >=6 else '可' if n>=3 else "不可" 
+result='優' if n>=9:  else: '良' n>=6 if n >=6  else: '可'  if n>=3 else: '不可'
 print(result)

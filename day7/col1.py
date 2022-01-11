caf1a001e0b369f565784b80090f89c70d9d4a97
hogehoge <susumitosusyo@hotmail.com>
2022-01-11 13:07:29 +0900

    通常の引数でのdictの受け渡しも追記

--- day7/col1.py
+++ day7/col1.py
@@ -3,3 +3,10 @@ def eat(**kwargs):
         print('{}に{}を食べました'.format(key,kwargs[key]))
 
 eat(朝食='納豆',遅めの昼食='パスタ',夕方のおやつ='カレーパン')
+
+def eat2(foods):
+    for key in foods:
+        print('{}に{}を食べました。'.format(key,foods[key]))
+
+foods={'朝食':'納豆','昼食':'ラーメン'};
+eat2(foods)
